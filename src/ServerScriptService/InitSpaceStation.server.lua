71b090a89c8ac6e465d6d2eae382683fad79104a
finintra <a.finberg@itransition.com>
2025-05-27 18:22:09 +0300

    Змінено InitSpaceStation на Server Script замість ModuleScript

--- src/ServerScriptService/InitSpaceStation.server.lua
+++ src/ServerScriptService/InitSpaceStation.server.lua
@@ -1,11 +1,13 @@
--- InitSpaceStation.lua
+-- InitSpaceStation.server.lua
 -- Скрипт для ініціалізації космічної станції при запуску гри
 
 local SpaceStationBuilder = require(script.Parent.SpaceStationBuilder)
 
 -- Ініціалізуємо станцію при завантаженні серверу
 local function init()
+	print("Починаю будівництво космічної станції...")
 	SpaceStationBuilder:BuildSpaceStation()
+	print("Космічну станцію збудовано успішно!")
 end
 
 -- Запускаємо ініціалізацію
